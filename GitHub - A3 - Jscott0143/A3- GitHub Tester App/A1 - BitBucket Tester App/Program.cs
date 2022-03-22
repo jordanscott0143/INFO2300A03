d64bdfc2044f17e14ead411c6c83d29e9f82914e
jordanscott0143 <102188192+jordanscott0143@users.noreply.github.com>
2022-03-22 16:55:06 -0400

    initial commit

---
+++ GitHub - A3 - Jscott0143/A3- GitHub Tester App/A1 - BitBucket Tester App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A1___BitBucket_Tester_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Hello Please enter a Name");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(" Hello " + input + "");
+
+            //made for testing a basic bit bucket build using GIT.
+
+
+
+        }
+    }
+}
